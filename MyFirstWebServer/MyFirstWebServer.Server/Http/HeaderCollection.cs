3580a6e1556f3ca9384934fce1682d04cc5e3140
NikolaDjerov <126071907+NikolaDjerov@users.noreply.github.com>
2026-02-10 08:59:35 +0200

    web

--- MyFirstWebServer/MyFirstWebServer.Server/Http/HeaderCollection.cs
+++ MyFirstWebServer/MyFirstWebServer.Server/Http/HeaderCollection.cs
@@ -10,7 +10,9 @@ namespace MyFirstWebServer.Server.Http
     {
         private readonly Dictionary<string, Header> headers;
         public HeaderCollection()
-            => this.headers = new Dictionary<string, Header>();
+        {
+            headers = new Dictionary<string, Header>();
+        }
 
         public int Count => this.headers.Count;
         public void Add(string name, string value)
